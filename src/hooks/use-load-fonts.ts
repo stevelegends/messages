2f7f9a6f051baa465668cc454c62fc8c5b74abc9
hiep <hiep@hieps-MacBook-Pro.local>
2023-09-06 23:37:12 +0700

    update load font

--- src/hooks/use-load-fonts.ts
+++ src/hooks/use-load-fonts.ts
@@ -1,22 +1,14 @@
+// react
+import {useCallback} from "react";
+
 // modules
-import {useFonts} from "expo-font";
+import Font, {useFonts} from "expo-font";
+
+// constants
+import FontMap from "../constants/font-map";
 
 const useLoadFonts = () => {
-    const [fontsLoaded, fontError] = useFonts({
-        'Roboto-Black': require('../../assets/fonts/Roboto-Black.ttf'),
-        'Roboto-BlackItalic': require('../../assets/fonts/Roboto-BlackItalic.ttf'),
-        'Roboto-Bold': require('../../assets/fonts/Roboto-Bold.ttf'),
-        'Roboto-BoldItalic': require('../../assets/fonts/Roboto-BoldItalic.ttf'),
-        'Roboto-Italic': require('../../assets/fonts/Roboto-Italic.ttf'),
-        'Roboto-Light': require('../../assets/fonts/Roboto-Light.ttf'),
-        'Roboto-LightItalic': require('../../assets/fonts/Roboto-LightItalic.ttf'),
-        'Roboto-Medium': require('../../assets/fonts/Roboto-Medium.ttf'),
-        'Roboto-MediumItalic': require('../../assets/fonts/Roboto-MediumItalic.ttf'),
-        'Roboto-Regular': require('../../assets/fonts/Roboto-Regular.ttf'),
-        'Roboto-Thin': require('../../assets/fonts/Roboto-Thin.ttf'),
-        'Roboto-ThinItalic': require('../../assets/fonts/Roboto-ThinItalic.ttf'),
-    });
-
+    const [fontsLoaded, fontError] = useFonts(FontMap);
     return {fontsLoaded, fontError}
 }
 
