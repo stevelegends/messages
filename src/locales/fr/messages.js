6c84e3219d3adb62d94a2e9a8e32cf981d7cde6e
hiep <hiep@hieps-MacBook-Pro.local>
2023-09-15 00:24:12 +0700

    locales

--- src/locales/fr/messages.js
+++ src/locales/fr/messages.js
@@ -1,5 +1,5 @@
 /*eslint-disable*/ module.exports = {
     messages: JSON.parse(
-        '{"mErq7F":"Sign Up","5lWFkC":"Sign in","8Q+lLG":"Chats","Tz0i8g":"Settings","AHZflp":"Chat","oh3k6m":"Email is a required","pHx2eA":"Email must be a valid email","gAzCPb":"Password is a required","O3oNi5":"Email","8ZsakT":"Password","f8NwaA":"Switch to sign up","J+PEVw":"First name is a required","Ichqrq":"Last name is a required","V1EGGU":"First name","1ZaQUH":"Last name"}'
+        '{"mErq7F":"Sign Up","5lWFkC":"Sign in","8Q+lLG":"Chats","Tz0i8g":"Settings","AHZflp":"Chat","oh3k6m":"Email is a required","pHx2eA":"Email must be a valid email","gAzCPb":"Password is a required","ETNx7m":"Password must Contain 6 Characters, One Uppercase, One Lowercase, One Number and One Special Case Character","O3oNi5":"Email","8ZsakT":"Password","f8NwaA":"Switch to sign up","bw/GcT":"Please enter valid first name","S+1EIY":"First name must be at most 40 characters","J+PEVw":"First name is a required","svRvSh":"Please enter valid last name","WP6ekV":"Last name must be at most 40 characters","Ichqrq":"Last name is a required","U06iSy":"Email must be a vali email","V1EGGU":"First name","1ZaQUH":"Last name"}'
     )
 };
